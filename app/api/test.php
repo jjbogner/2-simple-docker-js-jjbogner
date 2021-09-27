48a1ef9100cf36713addc3963d809cf299a908f9
jjbogner <jjbogner@iu.edu>
2021-09-27 09:16:27 -0400

    moved api folder into public folder

--- app/api/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$foo = "To be";
-$bar = "or not to be";
-
-echo $foo . $bar;
-
---
+++ app/public/api/test.php
@@ -0,0 +1,36 @@
+<?php
+
+$foo = "To be";
+$bar = "or not to be";
+
+echo $foo . ' ' . $bar . "\n";
+
+echo $num * $num * $num;
+
+$arr = [
+    "first" => "Tom",
+    "second" => "Bipin",
+    "best" => "DS"
+];
+
+$arr2 = [div 1,2,3,4,5,8]
+
+if (true) {
+    echo "\nTRUE\n"
+}
+
+while (true) {
+    //This way it doesn't actually do anything
+    break;
+}
+
+# This is also a comment
+/* This is a 
+multi-line
+comment */
+
+foreach($i:$arr){
+    echo $i;
+}
+
+
